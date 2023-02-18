dd23f30cf9161caa93a4dec5d7c19fecf46e8f22
Bengt Mayr <mayr.bengt@gmail.com>
2023-02-18 13:37:23 +0100

    Added SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+    modules: ['@nuxtjs/tailwindcss'],
+    head: {
+        title: 'DevBxnq - Developer',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Mein Name ist Bengt und ich programmiere Software. Du willst mich kontaktieren? Drücke auf den contact Knopf!'
+            },
+            {
+                property: 'og:type',
+                content: 'website'
+            },
+            {
+                property: 'og:url',
+                content: 'https://www.devbxnq.de'
+            }
+        ],
+        link: [{ rel: 'icon', type: 'image/png', href: '/assets/img/logo.png' }]
+    }
 })
